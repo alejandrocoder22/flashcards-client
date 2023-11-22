1056e88e2a5cf9639dbab5aea480e0f74428e364
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-11-22 07:06:08 +0100

    Added Header skeletoon and styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header className='main-header'>
+      <div className='Logo'>Logo</div>
+      <nav>
+        <ul className='main-header__ul'>
+          <li className='main-header__li'>Inicio</li>
+          <li className='main-header__li'>Flash Cards</li>
+          <li className='main-header__li'>Log In</li>
+        </ul>
+      </nav>
+    </header>
+
+  )
+}
+
+export default Header
