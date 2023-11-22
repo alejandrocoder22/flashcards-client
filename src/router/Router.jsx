39dcb0dbad0d9afefd87e4ef7f03ccb6c97afe41
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-11-22 07:35:23 +0100

    Added all routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import MyCards from '../pages/MyCards'
+import CommunityCards from '../pages/CommunityCards'
+import CreateCard from '../pages/CreateCard'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
 
 const Router = () => {
   return (
@@ -11,6 +16,11 @@ const Router = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/mis-tarjetas' element={<MyCards />} />
+          <Route path='/comunidad' element={<CommunityCards />} />
+          <Route path='/crear-tarjeta' element={<CreateCard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/registrarse' element={<Register />} />
         </Routes>
       </BrowserRouter>
       <Footer />
