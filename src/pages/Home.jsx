002164c5572a2ddaa9c6baed60a3b60245691cb9
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-11-27 10:49:58 +0100

    Added decks fetching and styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 
+const getPublicCardsByTopic = async (setDecks) => {
+  const response = await fetch('http://localhost:3001/api/decks?pagination=0&topic=Science')
+  const data = await response.json()
+  setDecks(data.decks)
+}
+
 const Home = () => {
+  const [decks, setDecks] = useState([])
+  useEffect(() => {
+    getPublicCardsByTopic(setDecks)
+  }, [])
   return (
     <div className='home'>
       <aside className='home__sidebar'>Aside</aside>
       <section className='home__cards-container'>
         <Header />
         <h2>Community topic flashcards</h2>
+        <div className="home__cards">
+          {
+            decks.map(deck =>   <p className="home__card" key={deck.deck_id}>{deck.deck_name}</p>)
+          }
+        </div>
       </section>
     </div>
   )
