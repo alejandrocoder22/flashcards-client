876f7b84b8e3be2c010eaaec670b48c3919bb4c2
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-01-05 18:10:34 +0100

    Added skeletoon and basic styles

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,30 @@
+import NoSidebarHeader from '../components/NoSidebarHeader'
+
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <NoSidebarHeader />
+      <section className='auth'>
+        <div className='auth__input-container'>
+          <label className='auth__label'>Username</label>
+          <input className='auth__input' />
+        </div>
+        <div className='auth__input-container'>
+          <label className='auth__label'>Email</label>
+          <input className='auth__input' />
+        </div>
+        <div className='auth__input-container'>
+          <label className='auth__label'>Password</label>
+          <input className='auth__input' />
+        </div>
+        <div className='auth__input-container'>
+          <label className='auth__label'>Repeat Password</label>
+          <input className='auth__input' />
+        </div>
+
+        <button className='auth__button'>Register</button>
+      </section>
+    </>
   )
 }
 
