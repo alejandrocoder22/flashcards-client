7a5264b56ba71e75eda012a9d171393d96d8f623
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-01-05 18:17:30 +0100

    Added handleForm logit to frontend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,21 +1,24 @@
 import NoSidebarHeader from '../components/NoSidebarHeader'
+import useForm from '../hooks/useForm'
 
 const Register = () => {
+  const { handleForm, form } = useForm()
+
   return (
     <>
       <NoSidebarHeader />
       <section className='auth'>
         <div className='auth__input-container'>
           <label className='auth__label'>Username</label>
-          <input className='auth__input' />
+          <input onChange={handleForm} name="username" className='auth__input' />
         </div>
         <div className='auth__input-container'>
           <label className='auth__label'>Email</label>
-          <input className='auth__input' />
+          <input onChange={handleForm} name="email" className='auth__input' />
         </div>
         <div className='auth__input-container'>
           <label className='auth__label'>Password</label>
-          <input className='auth__input' />
+          <input onChange={handleForm} name="password"  className='auth__input' />
         </div>
         <div className='auth__input-container'>
           <label className='auth__label'>Repeat Password</label>
