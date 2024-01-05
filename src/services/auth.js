05c964d5ed2ca138e012115f8911ca8ef2bfdfa5
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-01-05 18:27:03 +0100

    Added onRegister service

---
+++ src/services/auth.js
@@ -0,0 +1,13 @@
+  export const onRegister =  async (e, form) => {
+    e.preventDefault()
+    const petition = fetch(import.meta.env.VITE_BASE_URL + 'api/users/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form)
+    })
+    const response = await petition.json()
+
+    console.log(response)
+  }
