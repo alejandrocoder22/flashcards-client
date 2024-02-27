84858d142f82cc481dd998c42cce73c0a2bfa2e8
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-02-27 10:49:25 +0100

    Created login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,36 @@
+import useForm from '../hooks/useForm'
+import NoSidebarHeader from '../components/NoSidebarHeader'
+
 const Login = () => {
+
+  const {handleForm, form} = useForm()
+
+
+  const onLogin = (e) => {
+    e.preventDefault()
+    fetch(import.meta.env.VITE_BASE_URL + 'api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+       body: JSON.stringify(form)
+    })
+  }
   return (
-    <div>Login</div>
+      <>
+    <NoSidebarHeader/>
+      <form onSubmit={onLogin} className='auth'>
+        <div className='auth__input-container'>
+          <label className='auth__label'>Username</label>
+          <input onChange={handleForm} name='username' className='auth__input' />
+        </div>
+        <div className='auth__input-container'>
+          <label className='auth__label'>Password</label>
+          <input onChange={handleForm} name='password' type='password' className='auth__input' />
+        </div>
+        <button className='auth__button'>Login</button>
+      </form>
+    </>
   )
 }
 
